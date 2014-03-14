294d245559b0cec3fab5a5f66a66e86073fec16f
冯宇 <yu.feng@kinglandiot.com>
2014-03-14 13:26:34 +0800

    kinglandiot/jenkins: 新增git

--- kinglandiot/jenkins/start.sh
+++ kinglandiot/jenkins/start.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
+chmod 1777 /tmp
+
 #启动mysql
-/usr/sbin/mysqld_safe &
+chown -R mysql /var/lib/mysql
+/usr/bin/mysqld_safe &
 
 #启动mongodb
+chown -R mongodb /var/lib/mongodb
 /usr/bin/mongod -f /etc/mongodb.conf --fork
 
 #启动jenkins
+chown -R jenkins /var/lib/jenkins
 service jenkins start
 
 #启动ssh服务挂起
