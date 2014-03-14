7829a8040db77b1dc28fe40d079260280927b774
冯宇 <yu.feng@kinglandiot.com>
2014-03-14 18:59:50 +0800

    kinglandiot/jenkins: start.sh新增初始化数据库操作。删除不用的文件source.list

--- kinglandiot/jenkins/start.sh
+++ kinglandiot/jenkins/start.sh
@@ -4,7 +4,16 @@ chmod 1777 /tmp
 
 #启动mysql
 chown -R mysql /var/lib/mysql
-/usr/bin/mysqld_safe &
+if [ ! -f /var/lib/mysql/ibdata1 ]; then
+    mysql_install_db
+    /usr/bin/mysqld_safe &
+    sleep 2
+    echo "GRANT ALL ON *.* TO root@localhost IDENTIFIED BY 'root' WITH GRANT OPTION; FLUSH PRIVILEGES" | mysql
+    killall mysqld
+    sleep 10s
+else
+    /usr/bin/mysqld_safe &
+fi
 
 #启动mongodb
 chown -R mongodb /var/lib/mongodb
